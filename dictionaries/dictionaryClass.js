6f9088b90b5ff85e058c7f6c7b031f686bca91ba
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-09-01 16:48:31 +0100

    Auxiliary functions for the dictionary class

--- dictionaries/dictionaryClass.js
+++ dictionaries/dictionaryClass.js
@@ -4,6 +4,8 @@ function Dictionary () {
    this.find = find;
    this.remove = remove;
    this.showAll = showAll;
+   this.count = count;
+   this.clear = clear;
 }
 
 function add (key, value) {
@@ -27,10 +29,27 @@ function showAll () {
    }
 }
 
+function count() {
+   var n = 0;
+   for (var key in this.datastore) {
+      ++n;
+   }
+   return n;
+}
+
+function clear() {
+   for (var key in this.dataStore) {
+      delete this.dataStore[key]
+   }
+}
+
+
 var pbook = new Dictionary();
-pbook.add("Mike","123");
+pbook.add("Raymond","123");
 pbook.add("David", "345");
 pbook.add("Cynthia", "456");
+console.log("Number of entries: " + pbook.count());
 console.log("David's extension: " + pbook.find("David"));
-pbook.remove("David");
 pbook.showAll();
+pbook.clear();
+console.log("Number of entries: " + pbook.count());
